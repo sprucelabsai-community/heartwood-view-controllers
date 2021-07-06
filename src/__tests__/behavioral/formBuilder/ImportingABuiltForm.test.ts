79971a7292fce3bf904d303a9ee1bb5d7b18cc7f
Tay <me@taylorrome.ro>
2021-07-06 09:49:27 -0600

    lint

--- src/__tests__/behavioral/formBuilder/ImportingABuiltForm.test.ts
+++ src/__tests__/behavioral/formBuilder/ImportingABuiltForm.test.ts
@@ -2,9 +2,7 @@ import { test, assert } from '@sprucelabs/test'
 import { errorAssertUtil } from '@sprucelabs/test-utils'
 import AbstractViewControllerTest from '../../../tests/AbstractViewControllerTest'
 import EditFormBuilderSectionViewController from '../../../viewControllers/formBuilder/EditFormBuilderSection.vc'
-import FormBuilderViewController, {
-	FormBuilderImportExportObject,
-} from '../../../viewControllers/formBuilder/FormBuilder.vc'
+import FormBuilderViewController from '../../../viewControllers/formBuilder/FormBuilder.vc'
 
 export default class ImportingABuiltFormTest extends AbstractViewControllerTest {
 	protected static controllerMap = {
