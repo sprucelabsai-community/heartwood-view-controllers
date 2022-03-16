806f59b6089bfb573e18405ba7b02ed9e39ce206
Taylor Romero <me@taylorrome.ro>
2022-03-16 09:30:16 -0600

    patch: clicking cell progress

--- src/__tests__/behavioral/lists/ClickingACell.test.ts
+++ src/__tests__/behavioral/lists/ClickingACell.test.ts
@@ -1,11 +1,11 @@
 import { test, assert } from '@sprucelabs/test'
 import AbstractViewControllerTest from '../../../tests/AbstractViewControllerTest'
 import interactor from '../../../tests/utilities/interactor'
-class ClickingACell {}
 
 export default class ClickingACellTest extends AbstractViewControllerTest {
 	@test()
 	protected static async canCreateClickingACell() {
-		const err = await assert.doesThrowAsync(() => interactor.clickCell())
+		//@ts-ignore
+		await assert.doesThrowAsync(() => interactor.clickCell())
 	}
 }
