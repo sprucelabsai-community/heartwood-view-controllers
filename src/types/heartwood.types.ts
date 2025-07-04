d76649a2296ac365c6527f6125759d9f1196004f
Taylor Romero <me@taylorrome.ro>
2025-07-04 14:25:04 -0600

    patch: theme manager type fix

--- src/types/heartwood.types.ts
+++ src/types/heartwood.types.ts
@@ -295,7 +295,7 @@ export type InputButton =
     SpruceSchemas.HeartwoodViewControllers.v2021_02_11.InputButton
 
 export interface ThemeManager {
-    setTheme(theme: Theme): void
+    setTheme(theme: Theme, shouldAnimate?: boolean): void
     getTheme(): Theme
 }
 
