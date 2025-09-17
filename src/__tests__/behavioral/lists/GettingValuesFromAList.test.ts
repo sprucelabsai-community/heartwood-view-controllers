c7f8867b269615e7f4af1e8b29320c98926500fe
Maison damarante <maysondamarante@hotmail.com>
2025-09-17 17:51:05 -0300

    patch: vc -> this.vc

--- src/__tests__/behavioral/lists/GettingValuesFromAList.test.ts
+++ src/__tests__/behavioral/lists/GettingValuesFromAList.test.ts
@@ -2,26 +2,31 @@ import { test, suite, assert } from '@sprucelabs/test-utils'
 import { generateId } from '@sprucelabs/test-utils'
 import AbstractViewControllerTest from '../../../tests/AbstractViewControllerTest'
 import { RowValues } from '../../../types/heartwood.types'
+import ListViewController from '../../../viewControllers/list/List.vc'
 
 @suite()
 export default class GettingValuesFromAListTest extends AbstractViewControllerTest {
     private row = generateId()
     private name = generateId()
     private value = generateId()
+    private vc!: ListViewController
+
+    protected async beforeEach() {
+        await super.beforeEach()
+        this.vc = this.ListVc()
+    }
+
     @test()
     protected async valuesDropInIdOfRowsIfExist() {
-        const vc = this.ListVc()
-
-        const values = vc.getValues()
+        const values = this.vc.getValues()
         this.assertRowValuesEquals(values)
     }
 
     @test()
     protected async throwsIfSetValueIsMissingParams() {
-        const vc = this.ListVc(generateId())
         await assert.doesThrowAsync(
             //@ts-ignore
-            () => vc.setValue(),
+            () => this.vc.setValue(),
             'Options must have a row, name, and value.'
         )
     }
@@ -29,23 +34,20 @@ export default class GettingValuesFromAListTest extends AbstractViewControllerTe
     @test()
     protected async canSetValues() {
         const newValue = generateId()
-        const vc = this.ListVc()
-        await vc.setValue(this.row, this.name, newValue)
-        const values = vc.getValues()
+        await this.vc.setValue(this.row, this.name, newValue)
+        const values = this.vc.getValues()
         this.assertRowValuesEquals(values, newValue)
     }
 
     @test()
     protected async throwIfGetValuesMissingParams() {
-        const vc = this.ListVc(generateId())
         //@ts-ignore
-        assert.doesThrow(() => vc.getValue(), 'Row and name are required.')
+        assert.doesThrow(() => this.vc.getValue(), 'Row and name are required.')
     }
 
     @test()
     protected async canGetValue() {
-        const vc = this.ListVc()
-        const response = vc.getValue(this.row, this.name)
+        const response = this.vc.getValue(this.row, this.name)
         assert.isEqual(response, this.value)
     }
 
